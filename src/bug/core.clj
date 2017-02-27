59dbabf77f356fa75a861873bb69bad9a681385b
fs-rick <fs-rick@users.noreply.github.com>
2017-02-27 16:15:16 -0600

    Add punchline.

--- src/bug/core.clj
+++ src/bug/core.clj
@@ -7,5 +7,16 @@
    :satisfies? (satisfies? protocol x)}
   )
 
+;; Now eval this:
 
+(related? clojure.core.protocols/IKVReduce the-map)
 
+;; Problem is that Clojure 1.7 gives:
+
+{:extends? true, :satisfies? true}
+
+;; but Clojure 1.8 gives:
+
+{:extends? false, :satisfies? true}
+
+;; What gives?
