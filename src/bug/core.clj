587b3e0bec334645b4105fdfdf75e3dc9212900b
tom <tom@that-place>
2017-03-01 04:14:53 -0600

    Updated research

--- src/bug/core.clj
+++ src/bug/core.clj
@@ -3,7 +3,7 @@
 (def the-map {:a 2})
 
 (defn related? [protocol x]
-  {:extends? (extends? protocol (type x))
+  {:extends?   (extends? protocol (type x))
    :satisfies? (satisfies? protocol x)}
   )
 
@@ -20,3 +20,66 @@
 {:extends? false, :satisfies? true}
 
 ;; What gives?
+
+
+
+(defn super-chain [^Class c]
+  (when c
+    (cons c (super-chain (.getSuperclass c)))))
+
+;;Here's the source from clojure/src/clj/core_deftype.clj
+(defn implements? [protocol atype]
+  (and atype (.isAssignableFrom ^Class (:on-interface protocol) atype)))
+
+(defn my-extends? 
+  "Returns true if atype extends protocol"
+  {:added "1.2"}
+  [protocol atype]
+  (boolean (or (implements? protocol atype) 
+               (get (:impls protocol) atype))))
+
+;; I completely missed the fact that clojure.lang.IKVReduce popped up during
+;; the refactor, and is used as a fast-path for classes that implement it
+;; during kv-reduce.  
+
+;; I chased my tail trying to figure out why maps don't extend
+;; clojure.core.protocols/IKVReduce, which is simply because the interface
+;; clojure.core.protocols.IKVReduce is no longer implemented, leaving
+;; isAssignAbleFrom reflection call to return false now, while providing a
+;; protocol implementation for clojure.lang.IKVReduce.  The protocol
+;; implementation delegates to the class's implementation of
+;; clojure.lang.IKVReduce if possible.
+
+;; So, satisfies? traces through superclasses trying to find a protocol
+;; implementation, which is far more durable (depending on the protocol)
+;; rather than class-specific interface implementations I was using to
+;; cheat out a fastpath.  
+
+;; Just for grins, here's the microbenchmark associated with the "fast path"
+;; vs cached method lookup:
+
+(def the-map {:a 2})
+ 
+;;note the clojure.lang.KVReduce
+(defn ikv? [x] (instance? clojure.lang.KVReduce x))
+
+(let [cache (java.util.HashMap.)]
+  (defn sat? [protocol x]
+    (let [c (class x)]
+      (if-let [res (.get cache c)] ;we know.
+        res
+        (let [res (satisfies? protocol x)
+              _ (.put cache c res)]
+          res)))))
+  
+  
+;;Microbenchmarks.
+
+;;(time (dotimes [i 1000000] (sat? clojure.core.protocols/IKVReduce the-map)))
+;;"Elapsed time: 13.31948 msecs"
+
+;;(time (dotimes [i 1000000] (ikv? the-map))
+;;"Elapsed time: 6.816777 msecs"
+
+;Negligible cost for caching (preferably using a concurrent map in production).
+
